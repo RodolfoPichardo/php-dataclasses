2c073f9288f1349eb871ee404ec32c19ed19f830
Rodolfo Pichardo <rodolfopichardomena@gmail.com>
2022-07-04 00:47:23 -0400

    Partial basic functionality

---
+++ Dataclass.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace Dataclass;
+use InvalidArgumentException;
+use Generator;
+use JsonSerializable;
+use StdClass;
+
+class Dataclass implements JsonSerializable {
+    public function __construct(array $data) {
+        foreach($this->get_child_instance_variables() as $property) {
+            if (!array_key_exists($property, $data)) {
+                throw new InvalidArgumentException("Property $property is unexpectedly absent on the data supplied");
+            }
+            switch (gettype($data[$property])) {
+                case 'array':
+                    /*$class = ArrayTools\get_object_var_class($this, $property);
+                    if ($class === 'array') {
+                        $this->{$property} = $data[$property];
+                    } else {
+                        $this->{$property} = new $class($data[$property]);
+                    }
+                    break;*/
+                default:
+                    $this->{$property} = $data[$property];
+            }
+        }
+    }
+
+    private function get_child_instance_variables(): Generator {
+        $parent_vars = get_class_vars(__CLASS__);
+
+        foreach(get_class_vars($this::class) as $property => $_) {
+            if (!array_key_exists($property, $parent_vars)) {
+                yield $property;
+            }
+        }
+    }
+
+    public function jsonSerialize(): StdClass {
+        $obj = new stdClass();
+        foreach($this->get_child_instance_variables() as $prop) {
+            $obj->$prop = $this->$prop;
+        }
+
+        return $obj;
+    }
+
+    public function __debugInfo(): array {
+        $vars = [];
+        foreach($this->get_child_instance_variables() as $prop) {
+            $vars[$prop] = $this->$prop;
+        }
+
+        return $vars;
+    }
+}
+
---
+++ test/Primitives.php
@@ -0,0 +1,108 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+require_once("../Dataclass.php");
+use Dataclass\Dataclass;
+
+/**
+ *
+ */
+class EmptyClass extends Dataclass {
+}
+
+/**
+ *
+ */
+class IntegerClass extends Dataclass {
+    public int $num;
+}
+
+/**
+ *
+ */
+class StringClass extends Dataclass {
+    public string $str;
+}
+
+/**
+ *
+ */
+class FloatClass extends Dataclass {
+    public float $num;
+}
+
+/**
+ *
+ */
+class BooleanClass extends Dataclass {
+    public bool $yes_or_no;
+}
+
+/**
+ *
+ */
+class ClassWithDefaultValues extends Dataclass {
+    public float $num = 2.72;
+    public string $str = "Bye world";
+    public bool $yes_or_no;
+}
+
+
+
+
+final class Primitives extends TestCase
+{
+    public function testEmpty(): void {
+        $class = new EmptyClass([]);
+        $str = json_encode($class);
+
+        $this->assertSame("{}", $str);
+    }
+
+    public function testInteger(): void {
+        $class = new IntegerClass(["num" => 42]);
+
+        $this->assertTrue(isset($class->num), "Integer instance variable is not initialized");
+        $this->assertSame(42, $class->num);
+    }
+
+
+    public function testString(): void {
+        $class = new StringClass(["str" => "Hello world"]);
+
+        $this->assertTrue(isset($class->str), "String instance variable is not initialized");
+        $this->assertSame("Hello world", $class->str);
+    }
+
+    public function testFloat(): void {
+        $class = new FloatClass(["num" => 3.14]);
+
+        $this->assertTrue(isset($class->num), "Float instance variable is not initialized");
+        $this->assertSame(3.14, $class->num);
+
+        $class = new FloatClass(["num" => 3]);
+
+        $this->assertSame(3.0, $class->num);
+        $this->assertSame('double', gettype($class->num));
+    }
+
+    public function BooleanFloat(): void {
+        $class = new BooleanClass(["yes_or_no" => true]);
+
+        $this->assertTrue(isset($class->yes_or_no), "Boolean instance variable is not initialized");
+        $this->assertSame(true, $class->yes_or_no);
+    }
+
+    public function testDefaultValue(): void {
+        $class = new ClassWithDefaultValues(['yes_or_no' => true]);
+
+        $this->assertTrue(isset($class->yes_or_no), "Boolean instance variable is not initialized");
+        $this->assertSame(true, $class->yes_or_no);
+
+        $this->assertTrue(isset($class->num), "Boolean instance variable is not initialized");
+        $this->assertSame(2.72, $class->num);
+
+        $this->assertTrue(isset($class->str), "Boolean instance variable is not initialized");
+        $this->assertSame("Bye world", $class->str);
+    }
+}
